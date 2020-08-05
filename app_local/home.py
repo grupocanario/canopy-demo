ba130777fb6537ed0d1b3edd17c4cca31f3573e6
melissamnt <melissamnt10@gmail.com>
2020-08-05 09:33:53 -0500

    updated navbar

--- app_local/home.py
+++ app_local/home.py
@@ -7,7 +7,12 @@ from dash.dependencies import Input, Output
 from apps import alerta_sobrecosto, alerta_transparencia, visor_datos
 from importlib import import_module
 
-app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+app = dash.Dash(
+    __name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}]
+)
+
+server = app.server
 
 navbar_children = [
     dbc.NavItem(dbc.NavLink("Page 1", href="/page-1")),
@@ -18,61 +23,59 @@ navbar_children = [
 navbar = dbc.Navbar(navbar_children, sticky="top")
 
 
-# define content for page 2
-
-page2 = html.Div (
-    className='div-for-paragraphs text-secondary',
-    children=[
-        # Title
-        html.Div(
-            [
-                html.H2("Alerta: Sobrecosto"),
-            ],
-            className='text-left p-5'
-        ),
-        html.Div(
-            [
-                html.P(
-                    """
-                    En esta sección podrá acceder a los datos sobre alertas en la
-                    contratación destinada a atender la emergencia COVID-19 que posean sobrecostos,
-                    lo que quiere decir que se está adquiriendo un producto o servicio por encima
-                    del precio real.
-                    """
-                ),
-                html.P(
-                    """
-                    Como usuario puede realizar la búsqueda por las siguientes categorías:
-                    Ítem - Nombre de la entidad que contrata – Departamento – Municipio –
-                    Proveedor seleccionado – Valor del contrato – Precio por Item.
-
-
-                    """
-                ),
-                html.P(
-                    """
-                    Para iniciar la búsqueda debe escribir debajo del título de cada columna la palabra clave de interés.
-                    Por ejemplo en la columna Ítem: puede buscar elementos como kits de emergencia y le saldrán a nivel
-                    nacional los contratos realizados para adquirir este producto.
-                    Como otro ejemplo, si desea ver la contratación en su departamento o municipio, puede buscar escribiendo
-                    el nombre de su territorio debajo de la columna correspondiente.
-
-                    """
-                ),
-            ],
-            className='text-left p-5'
-        ),
-    ]
-)
-
 # define page layout
 app.layout = html.Div(
     [
         dcc.Location(id="url", pathname="/page-1"),
         navbar,
-        dbc.Container(id="content", style={"padding": "20px"}),
+        # Column for user controls (SIDE BAR)
+        html.Div(
+            className="row",
+            children=[
+                html.Div(
+                    className="three columns div-user-controls bg-grey",
+                    children=[
+                        html.Img(
+                            className="logo", src=app.get_asset_url("dash-logo-new.png")
+                        ),
+                        html.Div(
+                            [ 
+                                html.H3("COVID-19 - ALERTAS DE CONTRATACIÓN"),
+                            ],
+                            className='pb-5'
+                        ),
+                        html.Div(
+                            [ 
+                                html.P(
+                                    """
+                                    A través de esta plataforma podrá visualizar los contratos
+                                    suscritos para atender a la emergencia COVID-19 a nivel nacional y
+                                    territorial. En las diferentes secciones podrá encontrar alertas en
+                                    la contratación, estadísticas de la contratación pública y detalles sobre
+                                    la metodología del Proyecto Canopy.
+                                    """
+                                ),
+                            ],
+                            className='pt-3 pb-5'
+                        ),
+                        dcc.Markdown(
+                            children=[
+                                """Fuentes: [SECOP I](https://www.datos.gov.co/Presupuestos-Gubernamentales/SECOP-I-2020/c82b-7jfi) |
+                                [SECOP II](https://www.datos.gov.co/Gastos-Gubernamentales/SECOP-II-Contratos-Electr-nicos/jbjy-vk9h) |
+                                [Colombia Compra Eficiente](http://colombiacompra.gov.co/transparencia/api)
+                                """
+                            ]
+                        ),
+                    ],
+                ),
+                dbc.Container(
+                    id="content"
+                ),
+            ]
+        )
     ]
 )
+
 
 
 # create callback for modifying page layout
