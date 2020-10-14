4f0fa4c30ba4daca33d7f5be0e3c4b1c60994c01
Laura Melissa Montes Martin <melissamnt10@gmail.com>
2020-10-14 12:07:45 -0500

    added thumbnail

--- app_local/app.py
+++ app_local/app.py
@@ -1,8 +1,17 @@
 import dash
 
+metas = [{
+    "name": "viewport", 
+    "content": "width=device-width",
+    "og:title": "Proyecto Canopy",
+    "og:description": "Alertas tempranas para la contratación pública de COVID-19 en Colombia.",
+    "og:image": "https://raw.githubusercontent.com/grupocanario/canopy-demo/master/app_local/assets/logos/bird.png",
+    "og:url": "https://www.proyectocanopy.co/"
+}]
+
 app = dash.Dash(
     __name__, 
-    meta_tags=[{"name": "viewport", "content": "width=device-width"}], 
+    meta_tags=metas, 
     title='Canopy',
     update_title='Cargando...',
     suppress_callback_exceptions=True
