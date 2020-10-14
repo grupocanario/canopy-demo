935b0e18d69c5e9f0d9f92329408ed4eb29b018f
Laura Melissa Montes Martin <melissamnt10@gmail.com>
2020-10-14 12:39:44 -0500

    added thumbnail

--- app_local/app.py
+++ app_local/app.py
@@ -4,7 +4,7 @@ metas = [
     {"name": "viewport", "content": "width=device-width"},
     {"property": "og:title", "content": "Proyecto Canopy"}, 
     {"property": "og:description", "content": "Monitoreo de la contratación pública de COVID-19 en Colombia por medio de visualizaciones y alertas tempranas."}, 
-    {"property": "og:image", "content": "https://raw.githubusercontent.com/grupocanario/canopy-demo/master/app_local/assets/logos/Canopy_Black_Transparente-01.png"}, 
+    {"property": "og:image", "content": "https://raw.githubusercontent.com/grupocanario/canopy-demo/master/canopy_thumbnail.png"}, 
     {"property": "og:url", "content": "https://www.proyectocanopy.co/"}
 ]
 
